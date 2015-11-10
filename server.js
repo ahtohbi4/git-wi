b67903b02481bc22581738662e0b6fbfc4794be7
Alexander Antonov <alexandr-post@yandex.ru>
2015-11-10 22:58:35 +0300

    server.js was updated

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
+// @see: http://expressjs.com/4x/api.html
 var express = require('express'),
     app = express();
+var router = express.Router();
+// @see: https://github.com/expressjs/morgan
+var morgan = require('morgan');
+// @see: https://github.com/expressjs/compression
+var compression = require('compression');
 var path = require('path');
-var config = require('./vendors/config.js');
+var config = require('./vendors/config');
 var log = require('./vendors/log')(module);
 
 // app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
-// app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
-// app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-// app.use(express.methodOverride()); // поддержка put и delete
-// app.use(app.router); // модуль для простого задания обработчиков путей
-app.use(express.static(path.join(__dirname, 'public'))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
+app.use(morgan('combined'));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'compiled/')));
 
 app.get('/api', function (req, res) {
     res.send('API is running');
 });
 
-app.get('/ErrorExample', function (req, res, next){
-    next(new Error('Random error!'));
-});
-
-app.use(function (req, res, next){
+app.use(function (req, res){
     res.status(404);
-    log.debug('Not found URL: %s', req.url);
     res.send({
         error: 'Not found'
     });
@@ -29,9 +28,8 @@ app.use(function (req, res, next){
     return;
 });
 
-app.use(function (err, req, res, next){
+app.use(function (err, req, res){
     res.status(err.status || 500);
-    log.error('Internal error(%d): %s', res.statusCode, err.message);
     res.send({
         error: err.message
     });

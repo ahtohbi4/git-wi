ea9fa030acc673a4d133fc156235def55c3629d7
Alexander Antonov <alexandr-post@yandex.ru>
2015-11-19 23:47:35 +0300

    controller Validation was under construction

--- app/modules/Authorization/controller/authorization.js
+++ app/modules/Authorization/controller/authorization.js
@@ -1,11 +1,9 @@
 var Validation = require('./validation');
 
-var VALID_EMAIL = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-
 var Authorization = function () {
     var validation = new Validation();
 
-    return validation.check('a-a.a@a.aa', VALID_EMAIL);
+    return validation.checkEmail('a@a.aa');
 }
 
 // Authorization.prototype.
--- app/modules/Authorization/controller/validation.js
+++ app/modules/Authorization/controller/validation.js
@@ -1,13 +1,40 @@
+/**
+ * Validation
+ */
 var Validation = function () {}
 
-Validation.prototype.check = function (text, pattern) {
-    if (text === undefined) {
-        throw new Error('Required parameter was lost for method check(text, pattern) in:\n' + module.filename + '\n' + module.parent.filename);
+Validation.prototype._requiredParam = function (param) {
+    if (param === undefined) {
+        throw new Error();
+    } else {
+        return true;
     }
+}
+/**
+ * @method check
+ *
+ * @param {string} test
+ * @param {regexp} [pattern]
+ */
+Validation.prototype.check = function (test, pattern) {
+    this._requiredParam(test);
 
     var pattern = pattern || /.*/ig;
 
-    return pattern.test(text)
+    return pattern.test(test)
+}
+
+/**
+ * @method checkEmail
+ *
+ * @param {string} test
+ */
+Validation.prototype.checkEmail = function (test) {
+    this._requiredParam(test);
+
+    var PATTERN_EMAIL = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+    return PATTERN_EMAIL.test(test);
 }
 
 module.exports = Validation;

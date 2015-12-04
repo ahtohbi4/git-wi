0d7c31721fe722e6e56be7074fad7a8e569fa435
Alexander Antonov <alexandr-post@yandex.ru>
2015-12-04 23:40:27 +0300

    small fix in docs

--- libs/router.js
+++ libs/router.js
@@ -149,7 +149,7 @@ Router.prototype.getController = function(route) {
 };
 
 /**
- * @property FORMATS
+ * @const {object}
  * @see: https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_MIME-%D1%82%D0%B8%D0%BF%D0%BE%D0%B2
  */
 Router.prototype.FORMATS = {
